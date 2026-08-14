114b1ae6375421fdd5ac5407e52ef2fd1f5419cf
Jorge Luis Caviedes Alvarado <jlcaviedes1@gmail.com>
2026-08-13 21:19:12 -0500

    feat: created view regions

--- src/ui/views/RegionsView.vue
+++ src/ui/views/RegionsView.vue
@@ -1,10 +1,60 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Regions Pokemon')
+import spriteFish from "../../assets/images/jigglypuff.png";
 </script>
 
 <template>
-  <section >
-    <div> {{title}}</div>
-  </section>
+  <main class="regions">
+    <section class="regions__container">
+      <div>
+        <img class="regions__illustration" :src="spriteFish" alt="fish" />
+      </div>
+
+      <h1>¡Muy pronto disponible!</h1>
+      <p>
+        Estamos trabajando para traerte esta sección. Vuelve más adelante para
+        descubrir todas las novedades.
+      </p>
+    </section>
+  </main>
 </template>
+
+<style scoped>
+.regions {
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 24px;
+}
+
+.regions__container {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.regions__illustration {
+  width: 169px;
+  height: 210px;
+  object-fit: cover;
+}
+
+h1 {
+  margin-bottom: 16px;
+  font-size: 26px;
+  font-weight: 500;
+}
+
+p {
+  line-height: 1.6;
+  margin-bottom: 32px;
+  font-size: 14px;
+}
+</style>
